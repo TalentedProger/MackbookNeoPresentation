a8c4c2d37330bccf5da3f1e6cb048ab34ca735f1
Salim Sokurov <lasonandy@gmail.com>
2026-04-08 00:39:34 +0300

    +

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,23 +1,27 @@
-import express, { type Express } from "express";
+import express from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
-import router from "./routes";
-import { logger } from "./lib/logger";
+import type { IncomingMessage, ServerResponse } from "node:http";
+import router from "./routes/index.js";
+import { logger } from "./lib/logger.js";
 
-const app: Express = express();
+type PinoReq = IncomingMessage & { id?: string };
+type PinoRes = ServerResponse;
+
+const app = express();
 
 app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: PinoReq) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: PinoRes) {
         return {
           statusCode: res.statusCode,
         };
